07227cb423b3b0535b70c4895956e23fa52a056c
Mr. kuldeep <kkharoliya20@gmail.com>
2024-06-26 18:47:41 +0530

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='p-4 rounded-r-full bg-slate-700 text-center text-white font-bold font-mono'> Hello sir! welcome again to react js😄</h1>
+    </>
+  )
+}
+
+export default App
